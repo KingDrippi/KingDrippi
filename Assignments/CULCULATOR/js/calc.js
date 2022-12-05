c25f16998b473e352579fb6ab306679fb593435c
KingDrippi <115659523+KingDrippi@users.noreply.github.com>
2022-12-05 12:48:39 -0600

    ok

--- Assignments/CULCULATOR/js/calc.js
+++ Assignments/CULCULATOR/js/calc.js
@@ -11,3 +11,23 @@ console.log("Variable num represents the number: " + num);
 } 
 //Call the function
 resultAssign()
+
+//function to Add Two and Two
+//Declare
+function addTwoPlusTwo() {
+    result.innerHTML = 2 + 2;
+}
+
+//function to Subtract Two and Two
+//Declare
+function subtractTwoMinusTwo() {
+    result.innerHTML = 2 - 2;
+}
+
+//function to Divide Two and Two
+//Declare
+function divideTenDividedTwo() {
+    result.innerHTML = 10 / 2;
+}
+addTwoPlusTwo()
+
