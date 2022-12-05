7596468ba809b16d0e14a142f4cd7f3293e837e9
KingDrippi <115659523+KingDrippi@users.noreply.github.com>
2022-12-05 13:14:31 -0600

    ok

--- Assignments/CULCULATOR/js/calc.js
+++ Assignments/CULCULATOR/js/calc.js
@@ -18,16 +18,27 @@ function addTwoPlusTwo() {
     result.innerHTML = 2 + 2;
 }
 
-//function to Subtract Two and Two
-//Declare
-function subtractTwoMinusTwo() {
-    result.innerHTML = 2 - 2;
+//Multiplication
+function multiply(n1, n2) {
+    let multiplication = n1 * n2;
+    result.innerHTML = multiplication;
 }
 
-//function to Divide Two and Two
-//Declare
-function divideTenDividedTwo() {
-    result.innerHTML = 10 / 2;
+//Division
+function divide(n1, n2) {
+    let division = n1 / n2;
+    result.innerHTML = division;
+
 }
+
+//Subtraction
+function subtract(n1, n2) {
+    let subtraction = n1 - n2;
+    result.innerHTML = subtraction;
+}
+
+
+
+
 addTwoPlusTwo()
 
