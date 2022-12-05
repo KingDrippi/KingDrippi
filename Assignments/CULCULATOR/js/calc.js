b4002aecda28a704fcd68d9717db1a4c49e45966
KingDrippi <115659523+KingDrippi@users.noreply.github.com>
2022-12-05 11:58:28 -0600

    push

---
+++ Assignments/CULCULATOR/js/calc.js
@@ -0,0 +1,13 @@
+
+//Store Result in Global Variable
+const result = document.getElementById("answer");
+
+
+//Write a demo function 
+function resultAssign () {
+    num = 3 + 5 + 50 + 500 + 87 + 2;
+    result.innerHTML = num;
+console.log("Variable num represents the number: " + num);
+} 
+//Call the function
+resultAssign()
