e7ca6aadc07da0a62578d05114cf31bc3071edbb
KingDrippi <115659523+KingDrippi@users.noreply.github.com>
2022-12-06 10:40:06 -0600

    ok

---
+++ Assignments/LIGHTBOXGALLERY/js/lightbox.js
@@ -0,0 +1,20 @@
+let lightboxContainer = document.getElementById('lightboxContainer');
+let lightboxBackground = document.getElementById('lightboxBackground');
+let lightbox = document.getElementById('lightbox');
+let lightboxCloser = document.getElementById('lightboxCloser')
+
+//Function to open lightbox
+function openLightBox(){
+   //Turn on display class (from display:none to display: flex)
+   lightboxContainer.classList.add('display');
+   //Display image src inside lightbox
+lightbox.src = this.src;
+
+
+
+}
+
+
+let coffeeImage = document.getElementById('coffeeImage1');
+
+coffeeImage.onclick = openLightBox;
---
+++ CALCPRACTICE/js/practice.js
@@ -0,0 +1,11 @@
+//CHOOSE THE LARGER NUMBER
+function chooseLargerNumber(num1, num2) {
+ let larger;
+    if (num1 >= num2) {
+    larger = num1;
+
+     } else {
+     larger = num2;  
+     }
+     return larger;
+    }
