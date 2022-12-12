3cd23e4be50659d2265284bed9f28e0a35773fed
KingDrippi <115659523+KingDrippi@users.noreply.github.com>
2022-12-12 11:52:54 -0600

    ok

--- Assignments/GSAP1/js/gsap.js
+++ Assignments/GSAP1/js/gsap.js
@@ -7,3 +7,80 @@ stagger: 0.5,
 x:-500,
 y:-500,
 })
+
+// Squares Animation
+gsap.from(".chucked2", {
+    duration: 5,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x:-500,
+    y:-500,
+    })
+    // Squares Animation
+gsap.from(".chucked3", {
+    duration: 5,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x:-500,
+    y:-500,
+    })
+    // Squares Animation
+gsap.from(".chucked4", {
+    duration: 5,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x:-500,
+    y:-500,
+    })
+    // Squares Animation
+gsap.from(".chucked5", {
+    duration: 5,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x:-500,
+    y:-500,
+    })
+
+gsap.from(".chuck",{
+    duration: 5,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    opacity: 0,
+    x: 500,
+    y:250
+})
+
+//Text Animation 
+gsap.from('.headline', {
+    duration: 1.5,
+    stagger: 2.5,
+    opacity: 0,
+    y:150
+    
+}) 
+gsap.from(".first",{
+    duration: 2.5,
+    stagger: 1.5,
+    opacity: 0,
+    y:250
+})
+
+gsap.from(".second",{
+    duration: 4.5,
+    stagger: 2.5,
+    opacity: 0,
+    y:350
+})
+
+gsap.from(".third",{
+    duration: 5.5,
+    stagger: 3.5,
+    opacity: 0,
+    y:550
+    
+})
